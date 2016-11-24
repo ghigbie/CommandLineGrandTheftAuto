a966d905669667d456702028d717865bb026bfc2
George Higbie <gehigbie@gmail.com>
2016-11-24 15:49:22 -0500

    increased how much a cop charges

--- src/com/georgehigbie/Cop.java
+++ src/com/georgehigbie/Cop.java
@@ -9,7 +9,7 @@ public class Cop {
 
     public void bribe(Player player) {
         System.out.println("You bribed the cop and have to pay $50.");
-        player.Money-=50;
+        player.Money-=100;
         System.out.println("You have " + player.Money + " left. Sometimes da pimp'n life is hard.");
         System.out.println("");
     }
